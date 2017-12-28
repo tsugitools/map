fd3c460721fcca610cba8387d12327080eaf6c13
Charles Severance <csev@umich.edu>
2017-12-27 19:57:37 -0500

    Add more store info.

--- register.php
+++ register.php
@@ -4,6 +4,28 @@ $REGISTER_LTI2 = array(
 "name" => "Course Map",
 "FontAwesome" => "fa-map-marker",
 "short_name" => "Course Map",
-"description" => "This provides a simple Google map for a course where participants can indicate their location and control how much information they release to those viewing the map."
+"description" => "This provides a simple Google map for a course
+where participants can indicate their location and control how
+much information they release to those viewing the map.",
+    "privacy_level" => "public",  // anonymous, name_only, public
+    "license" => "Apache",
+    "languages" => array(
+        "English"
+    ),
+    "source_url" => "https://github.com/tsugitools/map",
+    "placements" => array(
+        "course_navigation"
+        /*
+        "course_navigation", "homework_submission",
+        "course_home_submission", "editor_button",
+        "link_selection", "migration_selection", "resource_selection",
+        "tool_configuration", "user_navigation"
+        */
+    ),
+    "screen_shots" => array(
+        "store/screen-01.png",
+        "store/screen-02.png",
+        "store/screen-03.png",
+        "store/screen-04.png"
+    )
 );
-
